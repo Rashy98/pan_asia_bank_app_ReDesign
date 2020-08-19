c3d998ac7c6216d8b4e32a22cc0cc5192b507f78
Rashy98 <rashinikavindya@gmail.com>
2020-08-19 14:01:07 +0530

    Initial commit - sideNav

---
+++ android/app/src/main/kotlin/chernobyl/com/pan_asia_bank_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package chernobyl.com.pan_asia_bank_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/NavDrawer.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class NavDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+          color: Color(0xFFB42827),
+        child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Container(
+            height: 100.0,
+          child: DrawerHeader(
+
+            child: Text(
+              'MANUJAYA'+"\n" + "manujayapremathilaka@gmail.com",
+              style: TextStyle(color: Colors.white, fontSize: 15),
+            ),
+          ),
+//            decoration: BoxDecoration(
+//                color: Colors.green,
+//                image: DecorationImage(
+//                    fit: BoxFit.fill,
+//                    )),
+          ),
+
+          ListTile(
+            leading: Icon(Icons.input),
+            title: Text('Welcome' , style: const TextStyle(
+              color: Color(0xFFFFFFFF)
+            ),
+            ),
+            onTap: () => {},
+
+          ),
+          ListTile(
+            leading: Icon(Icons.verified_user),
+            title: Text('Profile',style: const TextStyle(
+                color: Color(0xFFFFFFFF)
+            ),),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.settings),
+            title: Text('Settings',style: const TextStyle(
+                color: Color(0xFFFFFFFF)
+            ),),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.border_color),
+            title: Text('Feedback',style: const TextStyle(
+                color: Color(0xFFFFFFFF)
+            ),),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title: Text('Logout',style: const TextStyle(
+                color: Color(0xFFFFFFFF)
+            ),),
+            onTap: () => {Navigator.of(context).pop()},
+          ),
+        ],
+      )
+      ),
+    );
+  }
+}
